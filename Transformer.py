1302bcc27d5fb2f309fee0c53633a47f92d00ac1
Griffin <ghaisman@mit.edu>
2025-09-25 14:56:41 -0400

    Full architecture module

--- Transformer.py
+++ Transformer.py
@@ -1,19 +1,14 @@
 import numpy as np
 import math
 
+
 seq_len = 16     # number of time steps
 d_model = 128    # embedding dimension
 num_heads = 4
 d_ff = 256       # hidden size in FFN
 
-
 data = np.random.uniform(low=0, high=1, size=(seq_len, d_model))
 d_k = np.size(data, axis=0)
-
-"""Qw = np.random.uniform(low=-1, high=1, size=(d_model, d_model))
-Kw = np.random.uniform(low=-1, high=1, size=(d_model, d_model))
-Vw = np.random.uniform(low=0, high=1, size=(d_model, d_model))
-W0 = np.random.uniform(low=0, high=1, size=(d_model * num_heads, d_model))"""
 
 Qw = np.random.randn(d_model, d_model) / math.sqrt(d_model)
 Kw = np.random.randn(d_model, d_model) / math.sqrt(d_model)
@@ -33,29 +28,10 @@ def layernorm(x, eps=1e-6):
     std = x.std(axis=-1, keepdims=True)
     return (x - mean) / (std + eps)
 
-"""def QKV_Combine(Q, K, V):
-    QK = np.matmul(Q, np.transpose(K))
-    QK = QK / math.sqrt(d_model-1)
-    QK = softmax(QK)
-    QKV = np.matmul(QK, V)
-    return QKV
-
-def split_heads(x, num_heads):
-    # (seq_len, d_model) -> (num_heads, seq_len, d_head)
-    d_head = d_model // num_heads
-    return x.reshape(seq_len, num_heads, d_head).transpose(1, 0, 2)"""
-
 def combine_heads(x):
     # (num_heads, seq_len, d_head) -> (seq_len, d_model)
     num_heads, seq_len, d_head = x.shape
     return x.transpose(1, 0, 2).reshape(seq_len, num_heads * d_head)
-
-"""def singleattention(qw, kw, vw, tensor):
-    Q = np.matmul(qw, tensor)
-    K = np.matmul(kw, tensor)
-    V = np.matmul(vw, tensor)
-    Combined = QKV_Combine(Q,K,V)
-    return Combined"""
 
 
 def ffnn(inputlayer, wt1, wt2):
@@ -89,12 +65,28 @@ def multiheadattention(qw, kw, vw, w0, data):
     concat = combine_heads(attention)  # (seq, d_model)
     return concat @ w0
 
+
 def encoderlayer(qw, kw, vw, w0, w1, w2, data):
     weighted = multiheadattention(qw, kw, vw, w0, data)
     weighted = layernorm(weighted + data)
     linear = ffnn(weighted, w1, w2)
     linear = layernorm(linear + weighted)
     return linear
+
+
+def mlp(w0, w1, w2, w3, encoded):
+    encoded[:,-1] = 1
+    encoded = max(0,layernorm(encoded @ w0))
+    encoded = max(0,layernorm(encoded @ w1))
+    encoded = max(0,layernorm(encoded @ w2))
+    output = softmax(encoded @ w3)
+    return output
+
+
+def full_architecture(data, qw, kw, vw, w0, w1, w2, fw0, fw1, fw2, ow):
+    encoded = np.mean(encoderlayer(qw, kw, vw, w0, w1, w2, data), axis=0)
+    output = mlp(fw0, fw1, fw2, ow, encoded)
+    return output
 
 
 x = encoderlayer(Qw, Kw, Vw, W0, W1, W2, data) # 16x128
